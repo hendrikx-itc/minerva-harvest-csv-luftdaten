6929974f9d98a270fc6e95db7c27a9be3a07a1f6
Tommy van Schie <tommy.vanschie@hendrikx-itc.nl>
2019-04-04 16:01:21 +0200

    Added correct name for minerva-etl requirement

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ __author__ = "Hendrikx ITC"
 
 setup(
     name="minerva-harvest-csv",
-    version="1.0.0",
+    version="1.0.1",
     description=__doc__,
     author=__author__,
     author_email='info@hendrikx-itc.nl',
     python_requires='>=3.5',
-    install_requires=["minerva"],
+    install_requires=["minerva-etl>=5.0.0.dev2"],
     packages=["minerva_harvest_csv"],
     package_dir={"": "src"},
     entry_points={
