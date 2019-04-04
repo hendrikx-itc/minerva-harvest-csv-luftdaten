b7869f52c1b7fe9371ece7c2e76dffe09dd4ae00
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2019-04-04 16:26:20 +0200

    Rename package to reflect specific functionality for aireas data

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from setuptools import setup
 __author__ = "Hendrikx ITC"
 
 setup(
-    name="minerva-harvest-csv",
+    name="minerva-harvest-csv-aireas",
     version="1.0.1",
     description=__doc__,
     author=__author__,
     author_email='info@hendrikx-itc.nl',
     python_requires='>=3.5',
     install_requires=["minerva-etl>=5.0.0.dev2"],
-    packages=["minerva_harvest_csv"],
+    packages=["minerva_harvest_csv_aireas"],
     package_dir={"": "src"},
     entry_points={
-        "minerva.harvest.plugins": ["csv = minerva_harvest_csv:Plugin"],
-        "minerva.data_generator": ["csv = minerva_harvest_csv.generate:generate"]
+        "minerva.harvest.plugins": ["csv = minerva_harvest_csv_aireas:Plugin"],
+        "minerva.data_generator": ["csv = minerva_harvest_csv_aireas.generate:generate"]
     }
 )
