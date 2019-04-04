550f4f579e3b2a83b2766c1e62748266647e0f20
Tommy van Schie <tommy.vanschie@hendrikx-itc.nl>
2019-04-04 15:42:33 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from setuptools import setup
 __author__ = "Hendrikx ITC"
 
 setup(
-    name="minerva_harvest_csv",
+    name="minerva-harvest-csv",
     version="1.0.0",
     description=__doc__,
     author=__author__,
+    author_email='info@hendrikx-itc.nl',
+    python_requires='>=3.5',
     install_requires=["minerva"],
     packages=["minerva_harvest_csv"],
     package_dir={"": "src"},
